04bb1c6b84000b125f71e70c512ee4610dce520e
jiangkui <jiangkui@meituan.com>
2017-06-10 16:59:14 +0800

    2-6 搞定, 不仅要做完还要做好!

--- 2-6.c
+++ 2-6.c
@@ -1,47 +1,71 @@
 /**
  * 作者:jiangkui
  * 开始:2017年06月05日09:11:17
- * 结束:
- * 备注:
+ * 结束:2017年06月10日16:56:40
+ * 备注:最近一直在瞎忙活, 收收心, 继续搞, 后面会用心做, 做好。
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-//第p位是从右往左数
-unsigned setbits(int x, int p, int n, int y) {
+# define BIT_LENGTH 30
+
+/**
+ * 练习 2-6 :
+ *      编写一个函数 setbits(x, p, n, y), 该函数返回对 x 执行下列操作后
+ * 的结果值: 将 x 中从第 p 位开始的 n 个 (二进制) 位设置为 y 中最右边 n 位的值,
+ * x 的其余各位保持不变。
+ *
+ * 注意: 第p位是从右向左数, 并且假定最右边第一位是0位。
+ */
+
+int setbits(int x, int p, int n, int y) {
     return (x & ((~0 << (p + 1)) | (~(~0 << (p - n + 1))))) | ((y & ~(~0 << n)) << (p + 1 - n));
 }
 
-int main(){
-    unsigned i;
-    unsigned j;
-    unsigned k;
-    int p;
-    int n;
-//    char i2[32];
-//    char j2[32];
-//    char k2[32];
+/**
+ * 十进制转二进制
+ */
+void hexToBit(int i, char result[]){
+    int bit = 0;
+    for (int j = BIT_LENGTH-1; j >= 0; j--) {
+        if (i > 0) {
+            bit = i % 2;
+            i /= 2;
+            result[j] = '0' + bit;
+        } else {
+            result[j] = '0';
+        }
+    }
+}
 
-    for(i = 0; i < 30000; i += 511)
-    {
-        for(j = 0; j < 1000; j += 37)
-        {
-            for(p = 0; p < 16; p++)
-            {
-                for(n = 1; n <= p + 1; n++)
-                {
-                    //TODO 需要增加一个方法, 十进制转2进制。 以二进制的方式输出
-                    k = setbits(i, p, n, j);
-//                    itoa(i, i2, 2);
-//                    itoa(j, j2, 2);
-//                    itoa(k, k2, 2);
-//                    printf("setbits(%u, %d, %d, %u) = %u\n", i, p, n, j, k);
-                    printf("setbits(%o, %d, %d, %o) = %o\n", i, p, n, j, k);
-//                    printf("setbits(%s, %d, %d, %s) = %s\n", i2, p, n, j2, k2);
+/**
+ * 按照格式输出
+ */
+void formatPrint(int x, int p, int n, int y, int result){
+    char xBits[BIT_LENGTH], yBits[BIT_LENGTH], resultBits[BIT_LENGTH];
+
+    hexToBit(x, xBits);
+    hexToBit(y, yBits);
+    hexToBit(result, resultBits);
+
+    printf("x:%s, 从第 %d 位开始 %d 个, 替换成 y 最右边的 %d 个\n", xBits, p, n, n);
+    printf("y:%s\n", yBits);
+    printf("r:%s\n\n", resultBits);
+}
+
+int main(){
+    int result;
+    for (int x = 0; x < 1500; x += 511) {
+        for (int y = 0; y < 77; y += 37) {
+            for (int p = 0; p < 7; p += 3) {
+                for (int n = 1; n < p+1; n += 2) {
+                    result = setbits(x, p, n, y);
+                    formatPrint(x, p, n, y, result);
                 }
             }
         }
     }
     return 0;
 }
+
