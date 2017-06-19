b976464279509168aa4e758a6d7e7144e4d87434
jiangkui <jiangkui@meituan.com>
2017-06-19 10:10:04 +0800

    上交今天的作业

---
+++ five/5-7.c
@@ -0,0 +1,26 @@
+/**
+ * 作者:jiangkui
+ * 开始:2017年06月19日09:02:00
+ * 结束:
+ * 备注:
+ */
+
+/**
+ * 练习 5-7
+ *      重写函数 readlines, 将输入的文本存储到 main 函数提供的一个数组中, 而不是存储到 alloc 分配的内存空间中。
+ * 该函数的运行速度比改写前快多少?
+ */
+
+#define MAX_LEN 1000
+
+int readlines(){
+
+}
+
+
+int main(){
+    char lines[MAX_LEN];
+
+
+
+}
